e26b29f053aece36df48a383aedf95713d52e813
Dioni Alves <dionialves@gmail.com>
2017-11-10 15:26:35 -0200

    New version (mask_textinput) = 0.0.3

--- Examples/mask_textinput.py
+++ Examples/mask_textinput.py
@@ -1,73 +1,60 @@
 import kivy
+import re
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.textinput import TextInput
 
 kivy.require('1.10.0')
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 main_widget_kv = """
 #:import get_color_from_hex kivy.utils.get_color_from_hex
 
 FloatLayout:
-    CpfTextInput:
-        multiline: False
-        size_hint: .8, None
-        height: 33
-        pos_hint: {"center_x": .5, "center_y": .6}
-    FoneTextInput:
-        multiline: False
+    BoxLayout:
         size_hint: .8, None
         height: 33
         pos_hint: {"center_x": .5, "center_y": .5}
+        Label:
+            text: 'CPF:'
+            size_hint_x: .2
+            text_size: self.size
+            halign: 'left'
+            valign: 'middle'
+        CpfTextInput:
+            multiline: False
+            size_hint_x: 1.
+
 """
 
 
 class CpfTextInput(TextInput):
 
-    def insert_text(self, substring, from_undo=False):
-        s = substring
-
-        if len(self.text) < 13:
-            self.text = self.text.replace('.', '')
-
-            if len(self.text) > 8:
-                self.text = self.text[:9] + '.' + self.text[9:]
-                self.text = self.text[:6] + '.' + self.text[6:]
-                self.text = self.text[:3] + '.' + self.text[3:]
-            elif len(self.text) > 5:
-                self.text = self.text[:6] + '.' + self.text[6:]
-                self.text = self.text[:3] + '.' + self.text[3:]
-            elif len(self.text) > 2:
-                self.text = self.text[:3] + '.' + self.text[3:]
-
-        else:
-            self.text = self.text[:13]
-        return super(CpfTextInput, self).insert_text(s, from_undo=from_undo)
-
-
-class FoneTextInput(TextInput):
+    only_n = re.compile('[^0-9]')
 
     def insert_text(self, substring, from_undo=False):
-        s = substring
 
-        if len(self.text) < 13:
-            self.text = self.text.replace('.', '')
+        if len(self.text) < 14:
+            only_n = self.only_n
+            s = re.sub(only_n, '', substring)
+            cpf = self.text.replace('.', '')
 
-            if len(self.text) > 8:
-                self.text = self.text[:9] + '.' + self.text[9:]
-                self.text = self.text[:6] + '.' + self.text[6:]
-                self.text = self.text[:3] + '.' + self.text[3:]
-            elif len(self.text) > 5:
-                self.text = self.text[:6] + '.' + self.text[6:]
-                self.text = self.text[:3] + '.' + self.text[3:]
-            elif len(self.text) > 2:
-                self.text = self.text[:3] + '.' + self.text[3:]
+            if len(cpf) > 8:
+                cpf = cpf[:3] + '{0}' + cpf[3:6] + '{0}' + cpf[6:9] + '{1}' + cpf[10:]
+                cpf = cpf.format('.', '-')
+            elif len(cpf) > 5:
+                cpf = cpf[:3] + '{0}' + cpf[3:6] + '{0}' + cpf[6:]
+                cpf = cpf.format('.')
+            elif len(cpf) > 2:
+                cpf = cpf[:3] + '{0}' + cpf[3:]
+                cpf = cpf.format('.')
+
+            self.text = cpf
+            return super(CpfTextInput, self).insert_text(s, from_undo=from_undo)
 
         else:
-            self.text = self.text[:13]
-        return super(FoneTextInput, self).insert_text(s, from_undo=from_undo)
+            self.text = self.text[:14]
 
 
 class AlingApp(App):
