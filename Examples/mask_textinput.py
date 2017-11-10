a87f90bea19f1c440201052e8017171ccb60bc78
Dioni Alves <dionialves@gmail.com>
2017-11-09 22:52:40 -0200

    New version (mask_textinput) = 0.0.2

--- Examples/mask_textinput.py
+++ Examples/mask_textinput.py
@@ -5,11 +5,18 @@ from kivy.uix.textinput import TextInput
 
 kivy.require('1.10.0')
 
+__version__ = '0.0.2'
+
 main_widget_kv = """
 #:import get_color_from_hex kivy.utils.get_color_from_hex
 
 FloatLayout:
     CpfTextInput:
+        multiline: False
+        size_hint: .8, None
+        height: 33
+        pos_hint: {"center_x": .5, "center_y": .6}
+    FoneTextInput:
         multiline: False
         size_hint: .8, None
         height: 33
@@ -38,6 +45,29 @@ class CpfTextInput(TextInput):
         else:
             self.text = self.text[:13]
         return super(CpfTextInput, self).insert_text(s, from_undo=from_undo)
+
+
+class FoneTextInput(TextInput):
+
+    def insert_text(self, substring, from_undo=False):
+        s = substring
+
+        if len(self.text) < 13:
+            self.text = self.text.replace('.', '')
+
+            if len(self.text) > 8:
+                self.text = self.text[:9] + '.' + self.text[9:]
+                self.text = self.text[:6] + '.' + self.text[6:]
+                self.text = self.text[:3] + '.' + self.text[3:]
+            elif len(self.text) > 5:
+                self.text = self.text[:6] + '.' + self.text[6:]
+                self.text = self.text[:3] + '.' + self.text[3:]
+            elif len(self.text) > 2:
+                self.text = self.text[:3] + '.' + self.text[3:]
+
+        else:
+            self.text = self.text[:13]
+        return super(FoneTextInput, self).insert_text(s, from_undo=from_undo)
 
 
 class AlingApp(App):
